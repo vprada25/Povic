e6029beae6a732d277bfc7a40029f508986eeaa6
carlos95 <c.angulo@udla.edu.co>
2020-03-19 16:59:08 -0500

    feat: add http methods to make the api

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Post, Body } from "@nestjs/common";
 import { User } from "../../entities/user.entity";
 import {UserService} from "./user.service";
 
@@ -9,6 +9,15 @@ export class userController {
     @Get()
     async getUsers(): Promise<User[]> {
         return await this.userService.getUsers()
-
     }
+
+    @Get(":idUser")
+    async findUser(@Param("idUser")idUser): Promise<User>{
+        return await this.userService.findUser(idUser)
+    } 
+    
+    @Post()
+    async createUser(@Body()user): Promise<User>{
+        return await this.userService.createUser(user)
+    } 
 }
