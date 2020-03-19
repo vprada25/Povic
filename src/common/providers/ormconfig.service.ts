64b2d28e1cbe89654fa6603308264da290192fd7
carlos95 <c.angulo@udla.edu.co>
2020-03-19 16:57:37 -0500

    feat: add entitys user,person,role

--- src/common/providers/ormconfig.service.ts
+++ src/common/providers/ormconfig.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 import { ConfigService } from "@nestjs/config";
 import { User } from "../../entities/user.entity";
+import { Person } from "../../entities/person.entity";
+import { Role } from "../../entities/role.entity";
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-  constructor(private readonly config: ConfigService) {}
+  constructor(private readonly config: ConfigService) {
+    console.log(this.getOrmConfig())
+  }
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       ...this.getOrmConfig(),
       entities: [
-        User
+        User,
+        Person,
+        Role
       ],
     };
   }
@@ -22,7 +28,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       username: this.config.get('database.DB_USERNAME'),
       password: this.config.get('database.DB_PASSWORD'),
       database: this.config.get('database.DB_DATABASE'),
-      synchronize: this.config.get('database.DB_DATABASE'),
+      synchronize: this.config.get('database.BD_SYNCHRONIZE'),
     }
   }
 }
