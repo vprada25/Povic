16b050fea582f5b3205c48af34412947e1923981
carlos95 <c.angulo@udla.edu.co>
2020-03-19 17:01:03 -0500

    feat:impost userController,UserService in  user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { User } from "../../entities/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { userController } from "./user.controller";
+import { UserService } from "./user.service";
 
 
 @Module({
     imports:[TypeOrmModule.forFeature([User])],
-    controllers: [],
-    providers:[]
+    controllers: [userController],
+    providers:[UserService]
 })
 export class   UserModule{}
