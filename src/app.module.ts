497b463b41d9935b15269653076a76625dcdf4d5
carlos95 <c.angulo@udla.edu.co>
2020-03-19 16:50:13 -0500

    feat: remove module person

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from './common/common.module';
 import { UserModule } from './modules/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './common/providers/ormconfig.service';
-import { PerModule } from './modules/per/per.module';
+
+
 
 @Module({
-  imports: [CommonModule, 
+  imports: [CommonModule,
     UserModule,
     TypeOrmModule.forRootAsync({
       imports: [CommonModule],
       useExisting: TypeOrmConfigService,
-  
-}),
-    PerModule,],
+
+    }),
+    
+  ],
 })
-export class AppModule {}
+export class AppModule { }
